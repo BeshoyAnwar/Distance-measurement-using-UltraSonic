f7e82c6c5765b899805eeda839a9cf3ea13bdacf
Menna Mohamed Abdallah <32853257+MennaAbdallah@users.noreply.github.com>
2018-02-27 19:02:07 +0200

    Update display.c

--- display.c
+++ display.c
@@ -3,19 +3,19 @@ void displayOnSeg1 (char digit1)
 switch(digit1)
 	{
 	case '1': // A3 , A4
-					  GPIO_PORTA_DATA_R &= 0xE7; // 1110 01111
+					        GPIO_PORTA_DATA_R &= 0xE7; // 1110 01111
 						GPIO_PORTB_DATA_R &= 0xFF; // 1111 1111
 						break;
 	case '2': // A2 , A3 , A5 , A6 , B0
-					  GPIO_PORTA_DATA_R &= 0x93; // 1001 0011
+					        GPIO_PORTA_DATA_R &= 0x93; // 1001 0011
 						GPIO_PORTB_DATA_R &= 0xFE; // 1111 1110
 						break;
 	case '3': // A2 , A3 , A4 , A5 , B0
-					  GPIO_PORTA_DATA_R &= 0xC3; // 1100 0011
+					        GPIO_PORTA_DATA_R &= 0xC3; // 1100 0011
 						GPIO_PORTB_DATA_R &= 0xFE; // 1111 1110
 						break;
 	case '4': // A3 , A4 . A7 , B0
-					  GPIO_PORTA_DATA_R &= 0x67; // 0110 0111 
+					        GPIO_PORTA_DATA_R &= 0x67; // 0110 0111 
 						GPIO_PORTB_DATA_R &= 0xFE; // 1111 1110
 						break;
 	case '5': // A2 , A4 , A5 , A7 , B0
